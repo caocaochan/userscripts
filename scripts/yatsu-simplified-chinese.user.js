69cec008b650e2af37e57e303ddcca3a4d2e7e5b
caocaochan <mygthb@proton.me>
2026-06-04 21:49:34 +0200

    Raise Yatsu launcher and bump version

--- scripts/yatsu-simplified-chinese.user.js
+++ scripts/yatsu-simplified-chinese.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Yatsu Simplified Chinese
 // @namespace    https://app.yatsu.moe/
-// @version      0.1.0
+// @version      0.1.1
 // @updateURL    https://raw.githubusercontent.com/caocaochan/userscripts/main/scripts/yatsu-simplified-chinese.user.js
 // @downloadURL  https://raw.githubusercontent.com/caocaochan/userscripts/main/scripts/yatsu-simplified-chinese.user.js
 // @description  Converts Yatsu Reader book content from Traditional Chinese to Simplified Chinese with OpenCC.
@@ -59,7 +59,7 @@
     #${LAUNCHER_ID} {
       position: fixed;
       right: 24px;
-      bottom: 24px;
+      bottom: 72px;
       z-index: 2147483647;
       display: inline-flex;
       align-items: center;
@@ -92,7 +92,7 @@
     #${PANEL_ID} {
       position: fixed;
       right: 24px;
-      bottom: 82px;
+      bottom: 130px;
       z-index: 2147483647;
       box-sizing: border-box;
       width: min(320px, calc(100vw - 32px));
